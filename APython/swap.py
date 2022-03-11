ea05a7d3755cde1687a1d7ec09758442af8724d6
Anirudh R <89223726+Anirudh123nasty@users.noreply.github.com>
2022-03-11 23:26:00 +0000

    fixed swap

--- APython/swap.py
+++ APython/swap.py
@@ -7,6 +7,8 @@ def swap(age1, age2):
       age1 = temp2
       age2 = temp1
       return age1, age2
+  if age1 < age2:
+      return age1, age2
 
 def testSwap():
   firstAge = int(input("Enter a number for age 1:"))
