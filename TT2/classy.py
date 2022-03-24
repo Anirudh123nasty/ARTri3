275f2c0397b094d9fcd3e76926c1a1a6387d2071
Anirudh R <89223726+Anirudh123nasty@users.noreply.github.com>
2022-03-24 21:39:45 +0000

    fixed naming of factorial code

--- TT2/classy.py
+++ TT2/classy.py
@@ -1,17 +1,17 @@
-class Fibonacci:
+class Factorial:
     def __init__(self):
-        self.fiboSeq = [1]
+        self.factSeq = [1]
     def __call__(self, n):
-        if n < len(self.fiboSeq):
-            return self.fiboSeq[n]
+        if n < len(self.factSeq):
+            return self.factSeq[n]
         else:
             # Compute the requested Fibonacci number
-            fib_number = n * self(n - 1) # two recursive calls to self (__call__(self, n))
-            self.fiboSeq.append(fib_number) # builds list, with most nested of the calculations 1st... may hurt your head
-        return self.fiboSeq[n]
+            fact_number = n * self(n - 1) # two recursive calls to self (__call__(self, n))
+            self.factSeq.append(fact_number) # builds list, with most nested of the calculations 1st... may hurt your head
+        return self.factSeq[n]
 def facttester():
-  fibo_of = Fibonacci() # object instantiation and run __init__ method
+  fact_of = Factorial() # object instantiation and run __init__ method
   l = int(input('What number: '))
-  print(fibo_of(l)) # object running __call__ method
+  print(fact_of(l)) # object running __call__ method
   
 
