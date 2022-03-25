7bfb157798712e64907c4d3d8e5233073a27adc3
Anirudh R <89223726+Anirudh123nasty@users.noreply.github.com>
2022-03-25 21:18:51 +0000

    work done on 25th

--- TT2/math.py
+++ TT2/math.py
@@ -1,15 +1,10 @@
-# Function to find the Factors of a Number
-def findFactors(number):
-    print("Factors of a Given Number {0} are:".format(number))
-    for value in range(1, number + 1):
-        if number % value == 0:
-            print("{0}".format(value), end=" ")
-    print()
 
-def factors():
-    print("hello from factors")
-    num = int(input("Enter any Number to find its factors: "))
-    findFactors(num)
+num = int(input('Enter number: '))
 
-factors()
+print('The factors of', num, 'are:')
+for i in range(1, num+1):
+    if(num % i) == 0:
+        print(i, end=' ')
+
+
 
