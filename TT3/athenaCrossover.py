49c68ca736000bed9d3b4dd1a01b03ca2ef6bf2f
Athena9355 <gte782a@gmail.com>
2022-03-31 20:00:21 -0700

    program for user input.

--- TT3/athenaCrossover.py
+++ TT3/athenaCrossover.py
@@ -33,3 +33,57 @@ def game():
 
         # print user choice
         print("user choice is: " + choice_name)
+
+        # Computer chooses randomly any number among 1 , 2 and 3. Using randint method of random module
+        comp_choice = random.randint(1, 3)
+
+        #in case the user had the same choice as the computer
+        while comp_choice == choice:
+            comp_choice = random.randint(1, 3)
+
+        # initialize value of comp_choice_name
+        # variable corresponding to the choice value
+        if comp_choice == 1:
+            comp_choice_name = 'Rock'
+        elif comp_choice == 2:
+            comp_choice_name = 'paper'
+        else:
+            comp_choice_name = 'scissor'
+
+        print("Computer choice is: ", comp_choice_name)
+
+        print(choice_name, " v.s. ", comp_choice_name)
+
+        # condition for winning
+        if((choice == 1 and comp_choice == 2) or
+                (choice == 2 and comp_choice ==1 )):
+            print("paper wins => ", end = "")
+            result = "paper"
+
+        elif((choice == 1 and comp_choice == 3) or
+             (choice == 3 and comp_choice == 1)):
+            print("Rock wins =>", end = "")
+            result = "Rock"
+        else:
+            print("scissor wins =>", end = "")
+            result = "scissor"
+
+        # Printing either user or computer wins
+        if result == choice_name:
+            print(" User wins!!")
+        else:
+            print(" Computer wins!!")
+
+        print("Do you want to play again? (Y/N)")
+        ans = input()
+
+
+        # if user input n or N then condition is True
+
+        if ans == "n" or ans == "N":
+            break
+
+
+    # after coming out of the while loop
+    # we print thanks for playing
+    print("\nThanks for playing")
